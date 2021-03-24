58a68b4ddad11311e3606fd10599f2f2bd841289
aishwaryashelar3097 <imaishwaryashelar@gmail.com>
2021-03-24 21:51:37 +0530

    added Line Comparison

---
+++ src/module-info.java
@@ -0,0 +1,28 @@
+module lineComparision {
+	public class Line Compare{
+		public static void compare (line line_1, line line_2) {
+			if (line 1.equals(line 2)) {
+				System.out.println("Both lines are of equal length");
+			}
+			else if (line1.length of line > line 2.length of line) {
+				System.out.println("Line 1 is longer than line2")
+			}
+			else {
+				System.out.println("Line2 is longer than Line1");
+			}
+		}
+		public static void main (String[]args) {
+			System.out.println("welcome to Line Comparison Computation Program");
+			Print o = new Point(0,0);
+			Print a = new Point(3,4);
+			Print b = new Point(3,5);
+		line line1 = new line(0,a);
+		line line2 = new line(0,b);
+		System.out.println("Line 1: ");
+		Line1.print_line();
+		System.out.println("Line 2:");
+		Line2.print line();
+		compare_line_pair(line1,line2);
+		}
+	}
+}
